725d4f8ea22aa5ecc53b7af1eccc2b28d3492d1e
Aaron Jubbal <ajjubbal@gmail.com>
2014-10-16 23:55:08 -0700

    Initial (barebones) project commit.

---
+++ STATextField/STATextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STATextField
+//
+//  Created by Aaron Jubbal on 10/16/14.
+//  Copyright (c) 2014 Aaron Jubbal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
