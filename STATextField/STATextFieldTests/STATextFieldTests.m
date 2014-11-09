25a01c19bfad69357ca80d0b15b0d6b6bc0c077f
Aaron Jubbal <ajjubbal@gmail.com>
2014-11-09 14:41:59 -0800

    Set up kif.

--- STATextField/STATextFieldTests/STATextFieldTests.m
+++ STATextField/STATextFieldTests/STATextFieldTests.m
@@ -8,21 +8,21 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import <KIF/KIF.h>
 
-@interface STATextFieldTests : XCTestCase
+@interface STATextFieldTests : KIFTestCase
 
 @end
 
 @implementation STATextFieldTests
 
 - (void)setUp {
-    [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+    
 }
 
 - (void)testExample {
@@ -30,11 +30,5 @@
     XCTAssert(YES, @"Pass");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end
