93ba3bb896b0d8679e01667ebcf7effd6db5ccda
Aaron Jubbal <ajjubbal@gmail.com>
2014-11-10 23:58:37 -0800

    Implemented checks without using my new KIF methods (much better this way).

--- STATextField/STATextFieldTests/STATextFieldTests.m
+++ STATextField/STATextFieldTests/STATextFieldTests.m
@@ -28,8 +28,10 @@
 - (void)testExample {
     // This is an example of a functional test case.
     [tester enterText:@"1234" intoViewWithAccessibilityLabel:@"ATMTextField" traits:UIAccessibilityTraitNone expectedResult:@"12.34"];
-    [tester enterText:@"This is a test of\n" intoViewWithAccessibilityLabel:@"TextField" traits:UIAccessibilityTraitNone expectedResult:nil transitionsToViewWithAccessibilityLabel:@"NextTextField" andTraits:UIAccessibilityTraitNone];
+    [tester enterText:@"This is a test of\n" intoViewWithAccessibilityLabel:@"TextField"];
+    [tester waitForFirstResponderWithAccessibilityLabel:@"NextTextField"];
     [tester enterText:@"the emergency broadcast system.\n" intoViewWithAccessibilityLabel:@"NextTextField"];
+    [tester waitForAbsenceOfKeyboard];
 }
 
 
