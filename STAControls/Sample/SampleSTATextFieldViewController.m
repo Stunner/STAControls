387498f864431ce824c8b4f7431a5b1562effe3b
Aaron Jubbal <ajjubbal@gmail.com>
2015-06-13 17:18:33 -0700

    Sample project tests text field custom clear button states by changing the custom clear button to have a different image when highlighted.

--- STAControls/Sample/SampleSTATextFieldViewController.m
+++ STAControls/Sample/SampleSTATextFieldViewController.m
@@ -71,6 +71,8 @@
     self.nextTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
     [self.nextTextField setClearButtonImage:[UIImage imageNamed:@"Clear Button Image"]
                                    forState:UIControlStateNormal];
+    [self.nextTextField setClearButtonImage:[UIImage imageNamed:@"Clear Button Image 2"]
+                                   forState:UIControlStateHighlighted];
 //    _nextTextField.maxCharacterLength = 8;
 //    _nextTextField.currencyRepresentation = YES;
 //    _nextTextField.defaultValue = @"0.00";
